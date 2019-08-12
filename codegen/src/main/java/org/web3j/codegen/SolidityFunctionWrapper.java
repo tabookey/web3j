2de752ddb850961e9412fd57f8765190e809530a
shahafn <shahaflol@gmail.com>
2019-08-12 18:18:56 +0300

    Adding static functions to handle event parsing in generated contract java code
    Refactoring instance method to use the static one

--- codegen/src/main/java/org/web3j/codegen/SolidityFunctionWrapper.java
+++ codegen/src/main/java/org/web3j/codegen/SolidityFunctionWrapper.java
@@ -1126,9 +1126,33 @@ public class SolidityFunctionWrapper extends Generator {
                         .addParameter(TransactionReceipt.class, "transactionReceipt")
                         .returns(parameterizedTypeName);
 
+        transactionMethodBuilder.addStatement("return "
+                + "staticGet" + Strings.capitaliseFirstLetter(functionName) + "Events("
+                        + "transactionReceipt)");
+        return transactionMethodBuilder.build();
+    }
+
+    MethodSpec buildStaticEventTransactionReceiptFunction(
+            String responseClassName,
+            String functionName,
+            List<NamedTypeName> indexedParameters,
+            List<NamedTypeName> nonIndexedParameters) {
+
+        ParameterizedTypeName parameterizedTypeName =
+                ParameterizedTypeName.get(
+                        ClassName.get(List.class), ClassName.get("", responseClassName));
+
+        String generatedFunctionName =
+                "staticGet" + Strings.capitaliseFirstLetter(functionName) + "Events";
+        MethodSpec.Builder transactionMethodBuilder =
+                MethodSpec.methodBuilder(generatedFunctionName)
+                        .addModifiers(Modifier.PUBLIC, Modifier.STATIC)
+                        .addParameter(TransactionReceipt.class, "transactionReceipt")
+                        .returns(parameterizedTypeName);
+
         transactionMethodBuilder
                 .addStatement(
-                        "$T valueList = extractEventParametersWithLog("
+                        "$T valueList = staticExtractEventParametersWithLog("
                                 + buildEventDefinitionName(functionName)
                                 + ", "
                                 + "transactionReceipt)",
@@ -1185,6 +1209,10 @@ public class SolidityFunctionWrapper extends Generator {
         List<MethodSpec> methods = new ArrayList<>();
         methods.add(
                 buildEventTransactionReceiptFunction(
+                        responseClassName, functionName, indexedParameters, nonIndexedParameters));
+
+        methods.add(
+                buildStaticEventTransactionReceiptFunction(
                         responseClassName, functionName, indexedParameters, nonIndexedParameters));
 
         methods.add(
